394e410fd00efdeabc3d19ffb9cc40c156903aa5
Nick.Schnack <Nick.Schnack@veteransunited.com>
2022-02-09 10:59:16 -0600

    feat (todo): Add NgRx and actions to todo component

---
+++ my-ngrx-angular-app/src/app/todo/todo.actions.ts
@@ -0,0 +1,12 @@
+import { createAction, props } from '@ngrx/store';
+
+export const addTodo = createAction(
+    '[Todo Component] Add Todo',
+    props<{content: string}>()
+);
+
+export const deleteTodo = createAction(
+    '[Todo Component] Delete Todo',
+    props<{id: number}>()
+);
+
